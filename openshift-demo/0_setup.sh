1c0c2863add1aae45b49c8228d0310020011b1ab
alkesh-dagade <50432146+alkesh-dagade@users.noreply.github.com>
2019-05-09 17:32:46 +0530

    Delete 0_setup.sh

--- openshift-demo/0_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# oc login 10.1.2.2:8443 --username=admin --password=admin
-oc login -u system:admin
-
-# note: using minishift for the oc login
-# you can also "oc project kubedemo" to make the default namespace/project sticky
