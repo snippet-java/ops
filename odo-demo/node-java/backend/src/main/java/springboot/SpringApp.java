b60372ddf854b6ede883ed50ebf20079897559eb
alkesh-dagade <50432146+alkesh-dagade@users.noreply.github.com>
2019-06-27 16:51:32 +0530

    Create SpringApp.java

---
+++ odo-demo/node-java/backend/src/main/java/springboot/SpringApp.java
@@ -0,0 +1,41 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Spring Boot 2.x
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+ 
+// Spring Boot 1.x
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+  
+@SpringBootApplication
+public class SpringApp extends SpringBootServletInitializer {
+	
+	  public static void main(String[] args) {
+	        SpringApplication.run(applicationClass, args);
+	    }
+	  
+	    @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(applicationClass);
+	    }
+	  
+	    private static Class<SpringApp> applicationClass = SpringApp.class;
+	}
+	  
+	@RestController
+	class HelloController {
+	  
+	    @RequestMapping("/hello/{name}")
+	    String hello(@PathVariable String name) {
+	  
+	        return "Hi " + name + " !. This is response from spring boot web service.";
+	  
+	    }
+
+}
