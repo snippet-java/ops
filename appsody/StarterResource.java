a7df6bcc69822c207a92d730394c69f87e85de01
alkesh-dagade <50432146+alkesh-dagade@users.noreply.github.com>
2019-09-25 17:38:02 +0530

    Create StarterResource.java

---
+++ appsody/StarterResource.java
@@ -0,0 +1,10 @@
+package dev.appsody.starter;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+@Path("/resource")
+public class StarterResource {
+    @GET
+    public String getRequest() {
+        return "StarterResource response";
+    }
+}
